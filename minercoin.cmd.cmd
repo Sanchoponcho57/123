4e14a966c99ed74a1307a207c5708c79d66b5bf2
Sanchoponcho57 <pikalov_a@list.ru>
2024-01-24 23:43:05 +0300

    Add files via upload

---
+++ minercoin.cmd.cmd
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level 1 -o de.zephyr.herominers.com:1123 -u ZEPHs9J9BWM9YZy5xg8rif4bTqCPHWcdiBEjLiL2G8TVRz6UXV8pWyG9g43ma76n6MErEW7uaEXxHce1C2BUjyLdEjWY5Kax5NF -p SPMINER57 -a rx/0 -k 
+pause
